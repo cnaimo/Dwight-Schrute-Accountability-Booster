a2dd8c2be25c791344e95eef813ff1d1b91e066b
Christopher Naimo <55364182+cnaimo@users.noreply.github.com>
2020-06-02 10:43:47 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+attachment_dir = "email_attachment_docs/"
+accountability_email_subject = "Scranton Branch Consultant Report & Misc. Staff Emails"
+accountability_email_msg_file = "email_body.txt"
+office_stats_file = "office_stats.json"
+gmail_username = ""  # email account name from which the email is sent
+gmail_authentication = ""  # gmail password
+robert_california_email_addr = "" # address for email recipient
+
+flask_host_ip = '0.0.0.0'
+flask_port = 5432
